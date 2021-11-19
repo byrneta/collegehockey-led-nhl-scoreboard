cd1ce8c17f1d657d6bb78e2da95d4ec2d90fec41
byrneta <1069966+byrneta@users.noreply.github.com>
2021-11-19 18:10:16 -0500

    API fixes

--- collegehockey-led-scoreboard.py
+++ collegehockey-led-scoreboard.py
@@ -365,23 +365,23 @@ def displayLogos(awayTeam, homeTeam):
         # Load, crop, and resize the away team logo.
         awayLogo = Image.open("assets/images/team logos/png/" + awayTeam + ".png")
         awayLogo = cropImage(awayLogo)
-        awayLogo.thumbnail(logoSize)
+        awayLogo.thumbnail(logoSize, Image.ANTIALIAS)
     else:
         # Load, crop, and resize the away team logo.
         awayLogo = Image.open("assets/images/team logos/png/NCAA.png")
         awayLogo = cropImage(awayLogo)
-        awayLogo.thumbnail(logoSize)
+        awayLogo.thumbnail(logoSize, Image.ANTIALIAS)
 
     if os.path.exists("assets/images/team logos/png/" + homeTeam + ".png"):
         # Load, crop, and resize the home team logo.
         homeLogo = Image.open("assets/images/team logos/png/" + homeTeam + ".png")
         homeLogo = cropImage(homeLogo)
-        homeLogo.thumbnail(logoSize)
+        homeLogo.thumbnail(logoSize, Image.ANTIALIAS)
     else:
         # Load, crop, and resize the home team logo.
         homeLogo = Image.open("assets/images/team logos/png/NCAA.png")
         homeLogo = cropImage(homeLogo)
-        homeLogo.thumbnail(logoSize)
+        homeLogo.thumbnail(logoSize, Image.ANTIALIAS)
 
     # if not any(d['Team Abbreviation'] == awayTeam for d in getTeamData()):
     #     # Load, crop, and resize the away team logo.
